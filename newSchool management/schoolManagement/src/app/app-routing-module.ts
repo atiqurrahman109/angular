ae664ee5ea28c218bc4421c91b770df34b5a2a15
atiqurrahman109 <atiqurrahman.ar001@gmail.com>
2025-08-10 19:06:55 +0600

    adds

---
+++ newSchool management/schoolManagement/src/app/app-routing-module.ts
@@ -0,0 +1,25 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { AddStudentComponent } from './Component/add-student-component/add-student-component';
+import { ViewStudentComponent } from './Component/view-student-component/view-student-component';
+import { AddTeacherComponent } from './Component/add-teacher-component/add-teacher-component';
+import { ViewTeacherComponent } from './Component/view-teacher-component/view-teacher-component';
+import { Dashboard } from './layout/dashboard/dashboard';
+import { Login } from './Component/auth/login/login';
+import { Registration } from './Component/auth/registration/registration';
+
+const routes: Routes = [
+  {path: 'addStu', component: AddStudentComponent},
+  {path: 'viewAllStu', component: ViewStudentComponent},
+  {path:'addTeacher', component:AddTeacherComponent},
+  {path:'viewAllTeacher', component:ViewTeacherComponent},
+  {path:'', component:Dashboard},
+  {path:'login', component:Login},
+  {path:'reg', component:Registration}
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
---
+++ newSchool management/schoolManagement/src/app/service/teacher-service.ts
@@ -0,0 +1,40 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { TeacherModel } from '../model/teacher.model';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class TeacherService {
+  baseUrl: string = 'https://localhost:3000/teachers'; // Replace with your API
+
+  constructor(private http: HttpClient) {}
+
+  // Create Teacher
+  addTeacher(teacher: TeacherModel): Observable<TeacherModel> {
+    return this.http.post<TeacherModel>(this.baseUrl, teacher);
+  }
+
+  // Read All Teachers
+ getAllTeacher(): Observable<any> {
+
+    return this.http.get(this.baseUrl);
+
+  }
+
+  // Read Single Teacher
+  getTeacherById(id: number): Observable<TeacherModel> {
+    return this.http.get<TeacherModel>(`${this.baseUrl}/${id}`);
+  }
+
+  // Update Teacher
+  updateTeacher(id: number, teacher: TeacherModel): Observable<TeacherModel> {
+    return this.http.put<TeacherModel>(`${this.baseUrl}/${id}`, teacher);
+  }
+
+  // Delete Teacher
+  deleteTeacher(id: number): Observable<void> {
+    return this.http.delete<void>(`${this.baseUrl}/${id}`);
+  }
+}
